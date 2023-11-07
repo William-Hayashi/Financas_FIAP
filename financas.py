66b41d9706c63849c569a2fe9d284f09d57e6027
William-Hayashi <126543236+William-Hayashi@users.noreply.github.com>
2023-11-07 14:53:49 +0000

    commit will

--- financas.py
+++ financas.py
@@ -1,1 +1,17 @@
+def adicionar_transacao(tp_receita, valor, descricao, data):
+    new_transacao = {}
+    new_transacao ["tipo_receita"] = tp_receita
+    new_transacao ["value"] = valor
+    new_transacao ["description"] = descricao
+    new_transacao ["date"] = data
+    
 
+def remover_transacao ():
+    descricao = 
+    id = input(float("Qual o Id da transacao?"))
+    del descricao [descricao]
+   
+
+
+
+adicionar_transacao()
