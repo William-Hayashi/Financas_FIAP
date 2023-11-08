12cbfbeaba5d8a0b7fc80b449d968e2c4d3add62
William-Hayashi <126543236+William-Hayashi@users.noreply.github.com>
2023-11-08 00:22:05 +0000

    commit2

--- financas.py
+++ financas.py
@@ -1,10 +1,28 @@
-def adicionar_transacao(tp_receita, valor, descricao, data):
-    new_transacao = {}
-    new_transacao ["tipo_receita"] = tp_receita
-    new_transacao ["value"] = valor
-    new_transacao ["description"] = descricao
-    new_transacao ["date"] = data
-    
+def adicionar_transacao():
+    new_transacao = []
+    tp_receita = input("Digite o tipo de transação (receita ou despesa): ")
+    valor = float(input("Digite o valor: "))
+    descricao = input("Descreva a receita: ")
+    data = input("Digite a data da transação: ")
+
+    new_transacao.append(tp_receita)
+    new_transacao.append(valor)
+    new_transacao.append(descricao)
+    new_transacao.append(data)
+
+    print("CONFIRMAÇÃO DOS DADOS")
+    print(f"O tipo de receita é {tp_receita}.")
+    print(f"O valor colocado é {valor}.")
+    print(f"A descrição da transação é {descricao}")
+    print(f"E a data é {data}.")
+
+    resposta = input("Os dados estão corretos? (Responda com SIM ou NÃO)")
+    resposta_caixa_alta = resposta.upper()
+    if resposta_caixa_alta == "SIM":
+        print ("Okay! Os dados foram confirmados!")
+    else:
+        return adicionar_transacao()
+
 
 def remover_transacao ():
     descricao = 
